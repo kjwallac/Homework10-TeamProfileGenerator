494ff78a1c6b3e6e8a30729c978607ba1aef21d0
kjwallac <kristinajwallace@gmail.com>
2021-02-14 19:58:37 -0800

    begin unit test code for employee

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("getName", () => {
+        it("returns employee name", () => {
+            const name = "Jared";
+            
+            expect(result).toEqual(name);
+
+
+    })
+})
