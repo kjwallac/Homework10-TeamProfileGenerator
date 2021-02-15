26c4d084187a8b45d546f787f7dcc1542e0aba1d
kjwallac <kristinajwallace@gmail.com>
2021-02-14 21:18:02 -0800

    updated index.js node prompt to async function

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-inquirer
-    .prompt([
-      {
-          type: 'input',
-          name: 'name',
-          message: 'Enter employee name:',
-      },
-      {
-          type: 'list',
-          name: 'role',
-          message: "What is this employee's role?",
-          choices: ['Engineer', 'Manager', 'Intern'],
-      },
-      {
-          type: 'input',
-          name: 'email',
-          message: "Enter employee's email address:",
-      },
-    ])
-    .then((data) => {
-        const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-    
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-          err ? console.log(err) : console.log('Employee added!')
-        );
-      });
+async function run() {
+  const data = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter employee name:",
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is this employee's role?",
+      choices: ["Engineer", "Manager", "Intern"],
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter employee's email address:",
+    },
+  ]);
+
+  const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
+
+  fs.writeFileSync(filename, JSON.stringify(data, null, "\t"));
+}
+
+run();
